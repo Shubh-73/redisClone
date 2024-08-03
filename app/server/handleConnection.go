a861070b2c54636e9f261894908bd0e49fe0e033
Shubham Rajput <rajput.shubham37@gmail.com>
2024-08-03 20:18:17 +0530

    concurrent tests

--- app/server/handleConnection.go
+++ app/server/handleConnection.go
@@ -1,16 +1,42 @@
 package server
 
 import (
+	"bufio"
 	"log"
 	"net"
+	"strings"
 )
 
 func HandleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	_, err := conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		log.Println("Error writing response:", err)
+	reader := bufio.NewReader(conn)
+
+	for{
+
+		data, err := reader.ReadString('\n')
+		if err != nil{
+			log.Println("error while reading from connection: %v\n", err)
+			return
+		}
+
+		commands := strings.Split(strings.TrimSpace(data), "\n")
+		for _, command := range commands{
+			if command == "PING" {
+				_, err := conn.Write([]byte("PONG\r\n"))
+				if err != nil{
+					log.Println("error while writing to connection: %v\n", err)
+					return
+				}
+			}
+			else{
+				_, err := conn.Write([]byte("command unknown: " + command + "\r\n"))
+				if err != nil{
+					log.Println("error while writing to connection: %v\n", err)
+					return
+				}
+			}
+		}
 	}
 
 }
